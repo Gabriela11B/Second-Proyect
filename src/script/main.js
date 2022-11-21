d72825c6e2d61bcf7d0cb9b3cdc12d2a5f644644
Gabriela11B <beltran,g,0118@outlook.com>
2022-11-21 13:59:08 +0100

    update

---
+++ src/script/main.js
@@ -0,0 +1,72 @@
+/*  let btn = document.getElementById('btn')
+btn.addEventListener('click', function(){
+    local.storage.clear
+    
+
+ }) */ 
+ 
+ 
+
+/* if(localStorage.getItem('content1')){
+    const savedData = localStorage.getItem('content1')
+    content1.innerText= savedData 
+}
+     */
+    /*Esto del inicio lo intente usar profe, trato de entenderlo lo mejor que puedo, y ahi voy intentandolo, pero no supe como hacerlo, perdoneme*/
+
+    /*Lo que esta de aqui para abajo, fue basado en la clase del viernes, trate de interpretarlo de la misma forma, sin embargo, no tuve la oportunidad de ver las clases completas pero me esforce mucho por darle sentido y replicarlo para cada necesidad*/ 
+
+
+p1C1.addEventListener('click', function(){
+    let r1C1= prompt ('Insert Your Answer')
+    a1C1.innerText = r1C1
+    
+})
+
+
+p2C1.addEventListener('click', function(){
+    let r2C1 = prompt('Insert Your Answer')
+    a2C1.innerText = r2C1
+})
+p3C1.addEventListener('click', function(){
+    let r3C1 = prompt('Insert Your Answer')
+    a3C1.innerText = r3C1
+})
+
+
+p1C2.addEventListener('click', function(){
+    let r1C2= prompt ('Insert Your Answer')
+    a1C2.innerText = r1C2
+    
+})
+
+p2C2.addEventListener('click', function(){
+    let r2C2 = prompt('Insert Your Answer')
+    a2C2.innerText = r2C2
+})
+p3C2.addEventListener('click', function(){
+    let r3C2 = prompt('Insert Your Answer')
+    a3C2.innerText = r3C2
+})
+
+
+p1C3.addEventListener('click', function(){
+    let r1C3= prompt ('Insert Your Answer')
+    a1C3.innerText = r1C3
+    
+})
+
+p2C3.addEventListener('click', function(){
+    let r2C3 = prompt('Insert Your Answer')
+    a2C3.innerText = r2C3
+})
+p3C3.addEventListener('click', function(){
+    let r3C3 = prompt('Insert Your Answer')
+    a3C3.innerText = r3C3
+})
+
+
+
+
+
+
